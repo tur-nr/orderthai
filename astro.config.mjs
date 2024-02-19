97ebd6f919abdd2b56aa84e19e6f488977bd605e
Christopher Turner <turner296@gmail.com>
2024-02-19 16:10:36 +0700

    purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    {
+      name: "vite-plugin-purge-css",
+      hooks: {
+        ["astro:config:setup"]({ updateConfig }) {
+          updateConfig({
+            vite: {
+              plugins: [
+                pluginPurgeCss({
+                  keyframes: true,
+                  variables: true
+                })
+              ]
+            }
+          });
+        }
+      }
+    }
+  ]
+});
